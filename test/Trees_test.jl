87d76398de8b9b477d18c9ae3d451697f91c09bf
yfractal <mike@beansmile.com>
2015-04-14 15:53:32 +0800

    character test for getchild

--- test/Trees_test.jl
+++ test/Trees_test.jl
@@ -3,15 +3,21 @@ using FactCheck
 include("../src/Trees.jl")
 
 facts("Tree") do
-    context("ischild") do
-        t = Tree(:root)
-        insert!(t, [:a, :b, :c])
+    t = Tree(:root)
+    insert!(t, [:a, :b, :c])
 
+    context("ischild") do
         @fact ischild(:a, t) => true
 
         @fact ischild(:b, t) => false
         @fact ischild(:c, t) => false
         @fact ischild(:d, t) => false
+    end
+
+    context("getchild") do
+        @fact getchild(t, :a).value => :a
+
+        @fact getchild(t, :b) => nothing
     end
 end
 
